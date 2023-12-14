7d3439209aa57af7bed041384eeabf6887c25d10
Gabriel Zanon <gabriel.arantes@juntosenergia.com.br>
2023-12-14 03:08:27 -0300

    feat: ButtonAction - crete a computed property to handle styling and getting props

--- src/components/ButtonAction.vue
+++ src/components/ButtonAction.vue
@@ -1,13 +1,38 @@
 <script>
 export default {
-  name: 'ButtonAction'
+  name: 'ButtonAction',
+
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    buttonClass() {
+      return { [this.type]: true }
+    }
+  }
 }
 </script>
 
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Entrar
-  </button>
+  <button :class="buttonClass">{{ text }}</button>
 </template>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+.login {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+.search {
+  @apply hover:bg-brand-blue-2 bg-transparent text-brand-blue-1 hover:text-white;
+}
+</style>
