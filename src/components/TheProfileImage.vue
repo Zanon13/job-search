43c52636edb0085358506c77391bbaa83c18137b
Gabriel Zanon <gabriel.arantes@juntosenergia.com.br>
2023-12-13 21:28:39 -0300

    feat: TheProfileImage - create a component for profile image

---
+++ src/components/TheProfileImage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'TheProfileImage',
+
+  data: () => ({
+    imageLink: 'https://github.com/Zanon13.png'
+  })
+}
+</script>
+
+<template>
+  <img
+    :src="imageLink"
+    alt="Imagem de perfil do usuário"
+    class="h-8 w-8 rounded-3xl object-contain"
+  />
+</template>
