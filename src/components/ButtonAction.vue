48ec8324a5ef87e460fce54080d3d380f5062d18
Gabriel Zanon <gabriel.arantes@juntosenergia.com.br>
2023-12-13 21:19:25 -0300

    feat: ButtonAction - create a button component

---
+++ src/components/ButtonAction.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'ButtonAction'
+}
+</script>
+
+<template>
+  <button
+    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
+  >
+    Entrar
+  </button>
+</template>
